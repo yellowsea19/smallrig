3da2d5cdf85c7a603fa567ef8663f9624d03f054
huanghai <314221719@qq.com>
2023-07-04 09:18:20 +0800

    oss阿里云云掉KEY

--- common/tool/oss-up.py
+++ common/tool/oss-up.py
@@ -3,7 +3,7 @@ import oss2
 import os,sys
 
 # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-auth = oss2.Auth('LTAI5tNKh1StWRSV88vivH8L', '71Tx8OKTK0yYoHbs5etOuuKhBo7raI')
+auth = oss2.Auth('', '')
 # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 # 填写Bucket名称。
 bucket = oss2.Bucket(auth, 'https://oss-cn-shenzhen.aliyuncs.com', 'smallrig-test')
